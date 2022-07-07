fdc6a159390d151804b94ddde43ac039f1c19ee5
Vishwas <baghel.vishwas0@gmail.com>
2022-07-07 11:33:07 +0530

    [Vishwas]add: Temperature Conversion Program For Workshop Is Added

---
+++ WORKSHOP_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace WORKSHOP_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Temperature Conversion Program");
+            TemperatureConversion.CelciusToFarhenite();
+            TemperatureConversion.FarheniteToCelcius();
+        }
+    }
+}
---
+++ WORKSHOP_1/TemperatureConversion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WORKSHOP_1
+{
+    public class TemperatureConversion
+    {
+        public static void CelciusToFarhenite()
+        {
+            Console.Write("Enter Temperature In Celcius : ");
+            double celcius = Convert.ToDouble(Console.ReadLine());
+
+            double farhenite = (celcius * 9 / 5) +32;
+            Console.WriteLine("The Temperature In Farhenite : {0} ", farhenite);
+        }
+
+        public static void FarheniteToCelcius()
+        {
+            Console.Write("Enter Temperature In Farhenite : ");
+            double farhenite = Convert.ToDouble(Console.ReadLine());
+
+            double celcius = (farhenite - 32) * 5 / 9;
+            Console.WriteLine("The Temperature In Celcius : {0} ", celcius);
+        }
+
+    }
+}
